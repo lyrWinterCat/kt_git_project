65dcdcd444a6c61fa47cb5b820a171917ae4242e
WinterCat <dldulfla27@gmail.com>
2024-09-09 09:50:47 +0900

    coding masters start

---
+++ kt_aivle/src/basic/find_password.java
@@ -0,0 +1,25 @@
+package basic;
+
+// don't place package name.
+
+import java.io.*;
+
+import java.util.*;
+
+public class find_password {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+
+    }
+
+}
+// don't change 'Main' class name and  'public' accessor.
+
+
