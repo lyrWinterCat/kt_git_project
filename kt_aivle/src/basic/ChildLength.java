28d299bcb887897b789101735c5422100242ef56
WinterCat <dldulfla27@gmail.com>
2024-09-16 18:56:10 +0900

    자녀의 키는

---
+++ kt_aivle/src/basic/ChildLength.java
@@ -0,0 +1,18 @@
+package basic;
+
+import java.util.Scanner;
+
+public class ChildLength {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] numbers = input.split(" ");
+
+        int firstNumber = Integer.parseInt(numbers[0]);
+        int secondNumber = Integer.parseInt(numbers[1]);
+
+        System.out.println((firstNumber+secondNumber)/2);
+
+    }
+}
