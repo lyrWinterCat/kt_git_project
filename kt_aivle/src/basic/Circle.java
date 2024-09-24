60fdd697c9d793f06d6def1c04e8eb5d73e98298
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2024-09-24 09:06:50 +0900

    원의 넓이

---
+++ kt_aivle/src/basic/Circle.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        System.out.println(N*3.14);
+
+        scanner.close();
+    }
+}
